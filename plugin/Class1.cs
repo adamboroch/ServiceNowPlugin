f5d9786cd24fc42a18100ad811f5a070ffc94dab
edeklasin <adam.mateusz.boroch@gmail.com>
2025-08-30 22:48:30 +0200

    wprowadzam zmiany

--- plugin/Class1.cs
+++ plugin/Class1.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CyberArk.Extensions.Plugins.Models;
+using CyberArk.Extensions.Utilties.CPMParametersValidation;
+using CyberArk.Extensions.Utilties.Logger;
+
 
 namespace CPMPluginTemplate
 {
